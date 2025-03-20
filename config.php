c9d85587b7b008c0f4365f391d418155d3cebdec
albizup <alberto.riccardi13@gmail.com>
2025-03-20 15:33:00 +0100

    test config

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // Configurazione del database
 
-$db_host = $_ENV['db_host'];
-$db_name = $_ENV['db_name'];
-$db_user = $_ENV['db_user'];
-$db_pass = $_ENV['db_pass'];
+$db_host = getenv('db_host');
+$db_name = getenv('db_name');
+$db_user = getenv('db_user');
+$db_pass = getenv('db_pass');
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
